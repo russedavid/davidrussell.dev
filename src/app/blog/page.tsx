340dbba0d22ca5c8ededec71b6dd077a0ddab437
rijimuge <david.russell04@gmail.com>
2024-07-08 09:18:13 -0500

    change blog list to descending order

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,18 +6,7 @@ export default function Page() {
       <h1 className="py-2">
         In which thoughts, opinions and sometimes knowledge are shared.
       </h1>
-      <Link
-        href="blog/should-i-start-a-blog"
-        passHref
-        className="py-4 underline"
-      >
-        <h2 className="mb-2 text-xl font-semibold">Should I start a blog?</h2>
-      </Link>
-      <Link href="blog/quick-diarization" passHref className="py-4 underline">
-        <h2 className="mb-2 text-xl font-semibold">
-          Quick and easy Diarization
-        </h2>
-      </Link>
+
       <Link
         href="blog/improve-diarization-claude"
         passHref
@@ -26,6 +15,18 @@ export default function Page() {
         <h2 className="mb-2 text-xl font-semibold">
           nbdev and improving diarization with LLM
         </h2>
+      </Link>
+      <Link href="blog/quick-diarization" passHref className="py-4 underline">
+        <h2 className="mb-2 text-xl font-semibold">
+          Quick and easy Diarization
+        </h2>
+      </Link>
+      <Link
+        href="blog/should-i-start-a-blog"
+        passHref
+        className="py-4 underline"
+      >
+        <h2 className="mb-2 text-xl font-semibold">Should I start a blog?</h2>
       </Link>
     </div>
   );
